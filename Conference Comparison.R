3458673f5ac7f24c77469312081de1f8941979f8
Jeremy Dumalig <jeremydumalig@gmail.com>
2022-12-30 17:41:03 -0800

    More logos

--- Conference Comparison.R
+++ Conference Comparison.R
@@ -37,43 +37,18 @@ conference <-
   merge(logos, 
         by="Team")
 
-
-
-traditional <- 
-  read_csv("teams_traditional.csv") %>%
-  select(TEAM, GP, TOV)
-opponent <- 
-  read_csv("teams_opponent.csv") %>%
-  select(TEAM, `OPP\nTOV`)
-logos <-
-  read_csv("https://raw.githubusercontent.com/jeremydumalig/DataBank/master/logos.csv") %>%
-  select(TEAM, URL)
-
-teams <- 
-  merge(traditional, 
-        opponent,
-        by='TEAM') %>%
-  mutate(TOV = TOV / GP,
-         `OPP TOV` = `OPP\nTOV` / GP,
-         d_TOV = round(TOV - mean(TOV), 1),
-         d_OPPTOV = round(`OPP TOV` - mean(`OPP TOV`), 1)) %>%
-  select(TEAM, TOV, `OPP TOV`, d_TOV, d_OPPTOV) %>%
-  merge(logos,
-        by='TEAM')
-
-teams %>%
-  ggplot(aes(x=d_TOV,
-             y=d_OPPTOV)) +
-  geom_hline(yintercept=0, linetype="dashed") +
-  geom_vline(xintercept=0, linetype="dashed") +
+conference %>%
+  ggplot(aes(x=`DRB%`,
+             y=`ORB%`)) +
+  geom_hline(yintercept=mean(conference$`ORB%`), linetype="dashed") +
+  geom_vline(xintercept=mean(conference$`DRB%`), linetype="dashed") +
   geom_image(aes(image=URL),
              size=0.1,
              stat='identity') +
-  labs(title="Which teams commit and force the most turnovers?",
-       subtitle="NBA Regular Season | Through December 25, 2022",
-       caption="* Compared to League Average *",
-       x="Turnovers Per Game *",
-       y="Forced Turnovers Per Game") +
+  labs(title="Who are the best and worst rebounding teams?",
+       subtitle="UAA Preseason | Through December 30, 2022",
+       x="Offensive Rebound Rate (ORB%)",
+       y="Defensive Rebound Rate (DRB%)") +
   theme_linedraw() +
   theme(
     plot.margin = margin(1, 0.5, 0.5, 0.5, "cm"),
