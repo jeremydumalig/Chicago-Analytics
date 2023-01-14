38ee2045167b8d04c9c88fec0083fb000de2e349
Jeremy Dumalig <jeremydumalig@gmail.com>
2023-01-14 00:07:50 -0600

    uaa 1/13

--- Conference.R
+++ Conference.R
@@ -12,25 +12,27 @@ mbb_games <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1BcIP7CIYDTNnedc
 wbb_games <- gsheet2tbl("https://docs.google.com/spreadsheets/d/12JWqAMfVrSZobLohmxQK6PyrbNbOQbM6ux4LxXqWenM/edit#gid=1703250336")
 logos <- read_csv("https://raw.githubusercontent.com/jeremydumalig/Chicago-Analytics/main/uaa_logos.csv")
 
+date <- "January 7, 2023"
+
 if (women) {
   logs <- wbb_logs
   games <- wbb_games
-  subtitle <- "UAA Women's Basketball | Through January 7, 2023"
+  subtitle <- paste("UAA Women's Basketball | Through", date)
 } else {
   logs <- mbb_logs
   games <- mbb_games
-  subtitle <- "UAA Men's Basketball | Through December 30, 2022"
+  subtitle <- paste("UAA Men's Basketball | Through", date)
 }
 
 logs <-
   logs %>%
   filter(Opponent == "Total") %>%
-  select(Team, `ORB%`, `DRB%`)
+  select(Team, `ORB%`, `DRB%`, `TO%`, `OPP TO%`)
 games <-
   games %>%
   filter(Game == "Total") %>%
   mutate(Team = "Chicago") %>%
-  select(Team, `ORB%`, `DRB%`)
+  select(Team, `ORB%`, `DRB%`, `TO%`, `OPP TO%`)
 
 conference <- 
   rbind(logs, games) %>%
@@ -49,6 +51,31 @@ conference %>%
        subtitle=subtitle,
        x="Defensive Rebound Rate (DRB%)",
        y="Offensive Rebound Rate (ORB%)") +
+  theme_linedraw() +
+  theme(
+    plot.margin = margin(1, 0.5, 0.5, 0.5, "cm"),
+    plot.background = element_rect(fill = "grey90",
+                                   color = "black"),
+    legend.box.background = element_rect(size=0.75),
+    axis.title.x = element_text(size=14),
+    axis.title.y = element_text(size=14),
+    plot.title = element_text(size=18,
+                              face="bold"),
+    plot.subtitle = element_text(size=14),
+    plot.caption = element_text(size=10))
+
+conference %>%
+  ggplot(aes(x=`TO%`,
+             y=`OPP TO%`)) +
+  geom_hline(yintercept=mean(conference$`TO%`), linetype="dashed") +
+  geom_vline(xintercept=mean(conference$`OPP TO%`), linetype="dashed") +
+  geom_image(aes(image=URL),
+             size=0.1,
+             stat='identity') +
+  labs(title="Which teams commit and force the most turnovers?",
+       subtitle=subtitle,
+       x="Team Turnover Rate (TO%)",
+       y="Opponent Turnover Rate (OPP TO%)") +
   theme_linedraw() +
   theme(
     plot.margin = margin(1, 0.5, 0.5, 0.5, "cm"),
